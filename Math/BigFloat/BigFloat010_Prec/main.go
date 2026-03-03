f8fd498242a9f573979a9ea05ab7a0e6a2c704aa
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2026-03-02 23:30:36 -0600

    Added new method for computing big.Float precision

--- Math/BigFloat/BigFloat010_Prec/main.go
+++ Math/BigFloat/BigFloat010_Prec/main.go
@@ -29,13 +29,13 @@ func TestPrec001() {
   //               Digits    12345678901234567890123456789012345
   expectedResultStr := "3020.27536989418403462317254679710941519"
 
-  requiredFractionalDigits := 35
+  requiredFractionalDigits := uint(35)
 
-  exponentValue := 6
+  exponentValue := uint(6)
 
   baseStr := "3.14159"
 
-  requiredFloatPrecision := ComputeBigFloatPrecisionBits(35, exponentValue)
+  requiredFloatPrecision := ComputeBigFloatPrecisionBits(requiredFractionalDigits, exponentValue)
 
   baseFloat, isOk :=
     new(big.Float).
@@ -59,11 +59,11 @@ func TestPrec001() {
     return
   }
 
-  for i := 0; i < exponentValue; i++ {
+  for i := uint(0); i < exponentValue; i++ {
     resultBaseToExponent.Mul(resultBaseToExponent, baseFloat)
   }
 
-  actualResultStr := resultBaseToExponent.Text('f', requiredFractionalDigits)
+  actualResultStr := resultBaseToExponent.Text('f', int(requiredFractionalDigits))
 
   fmt.Printf("\n%v Base: %v\n",
     spacerStr,
@@ -81,17 +81,47 @@ func TestPrec001() {
 
 }
 
-func ComputeBigFloatPrecisionBits(decDigits int, multCount int) uint {
+// ComputeBigFloatPrecisionBits
+//
+//	This function computes the number of bits required to represent a
+//	floating-point number with the specified number of decimal digits.
+//
+//	The function uses the following formula to compute the number of bits:
+//
+//	baseBits = decDigits * log2(10) + safety
+//
+//	where:
+//
+//	decDigits - The number of decimal digits to be represented.
+//
+//	log2(10) - The base-2 logarithm of 10.
+//
+//	safety - A safety margin for exponentiation.
+//
+//	Important:
+//
+//	Remember, You must compute the total decimal digits (decDigits) in
+//	the floating point number to be supported with the computed and
+//	returned precision value.
+//
+//	Input Parameters:
+//
+//	resultDecDigits - The number of decimal digits to be represented.
+//
+//	multiplyCount - The number of times the base number multiplies by
+//	itself.
+func ComputeBigFloatPrecisionBits(resultDecDigits uint, multiplyCount uint) uint {
 
   // Convert decimal digits → bits
   // baseBits := float64(decDigits) * math.Log2(10)
-
+  //
+  // math.Log2(10) =
   //  3.321 928 094 887 362 347 870 319 429 489 390 175 864 831 393 024 6
-  // float64 max 15-digits of precision
-  baseBits := float64(decDigits) * 3.321928094887362
+  // float64 maximum 15-digits of precision
+  baseBits := float64(resultDecDigits) * 3.321928094887362
 
   // Add safety margin for exponentiation
-  safety := 64.0 + (float64(multCount) * 4.0)
+  safety := 64.0 + (float64(multiplyCount) * 4.0)
 
   // Round up to nearest whole bit
   return uint(math.Ceil(baseBits + safety))
