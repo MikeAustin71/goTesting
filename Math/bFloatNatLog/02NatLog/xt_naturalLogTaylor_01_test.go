91816bc9c90b004ef14b6257088da7a9fbd0ad2f
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2026-03-07 03:46:06 -0600

    Added new test

--- Math/bFloatNatLog/02NatLog/xt_naturalLogTaylor_01_test.go
+++ Math/bFloatNatLog/02NatLog/xt_naturalLogTaylor_01_test.go
@@ -1,64 +1,129 @@
 package naturalLogCalcs
 
 import (
-	"math/big"
-	"testing"
+  "math/big"
+  "testing"
 )
 
 func Test_Natural_Logarithm_Taylor_01(t *testing.T) {
 
-	ePrefix := "Test_Natural_Logarithm_Taylor_01()"
+  ePrefix := "Test_Natural_Logarithm_Taylor_01()"
 
-	xValueStr := "3237"
+  xValueStr := "3237"
 
-	xValue, isOk := new(big.Float).
-		SetMode(big.AwayFromZero).
-		SetString(xValueStr)
+  xValue, isOk := new(big.Float).
+    SetMode(big.AwayFromZero).
+    SetString(xValueStr)
 
-	if !isOk {
-		t.Errorf("%v\n"+
-			"Error returned by:\n"+
-			"xValue, isOk := new(big.Float)\n"+
-			".SetMode(big.AwayFromZero).SetString(xValueStr)\n"+
-			"xValueStr= '%v'\n"+
-			"Error= 'SetString() FAILED!'\n\n", ePrefix, xValueStr)
-		return
-	}
+  if !isOk {
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "xValue, isOk := new(big.Float)\n"+
+      ".SetMode(big.AwayFromZero).SetString(xValueStr)\n"+
+      "xValueStr= '%v'\n"+
+      "Error= 'SetString() FAILED!'\n\n", ePrefix, xValueStr)
+    return
+  }
 
-	// Fractional Digits ---------1---------2---------3
-	//         Accuracy: 123456789012345678901234567890
-	expectedResult := "8.082402253926244350924204901779"
+  // Fractional Digits ---------1---------2---------3
+  //         Accuracy: 123456789012345678901234567890
+  expectedResult := "8.082402253926244350924204901779"
 
-	prec := uint(7000)
+  prec := uint(7000)
 
-	actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)
+  actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)
 
-	if err != nil {
+  if err != nil {
 
-		t.Errorf("%v\n"+
-			"Error returned by:\n"+
-			"actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)\n"+
-			"xValue= '%v'\n"+
-			"prec= '%v'\n"+
-			"Error= '%v'\n\n",
-			ePrefix,
-			xValue.Text('f', 30),
-			prec,
-			err.Error())
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)\n"+
+      "xValue= '%v'\n"+
+      "prec= '%v'\n"+
+      "Error= '%v'\n\n",
+      ePrefix,
+      xValue.Text('f', 30),
+      prec,
+      err.Error())
 
-		return
-	}
+    return
+  }
 
-	actualResultStr := actualResult.Text('f', 30)
+  actualResultStr := actualResult.Text('f', 30)
 
-	if expectedResult != actualResultStr {
-		t.Errorf("%v\n"+
-			"Error: Unexpected Result!\n"+
-			"Because expectedResult != actualResultStr\n"+
-			"Expected actualResultStr = '%v'\n"+
-			"  Actual actualResultStr = '%v'\n\n",
-			ePrefix, expectedResult, actualResultStr)
-	}
+  if expectedResult != actualResultStr {
+    t.Errorf("%v\n"+
+      "Error: Unexpected Result!\n"+
+      "Because expectedResult != actualResultStr\n"+
+      "Expected actualResultStr = '%v'\n"+
+      "  Actual actualResultStr = '%v'\n\n",
+      ePrefix, expectedResult, actualResultStr)
+  }
 
-	return
+  return
+}
+
+func Test_Natural_Logarithm_Taylor_02(t *testing.T) {
+
+  ePrefix := "Test_Natural_Logarithm_Taylor_02()"
+
+  xValueStr := "245"
+
+  xValue, isOk := new(big.Float).
+    SetMode(big.AwayFromZero).
+    SetString(xValueStr)
+
+  if !isOk {
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "xValue, isOk := new(big.Float)\n"+
+      ".SetMode(big.AwayFromZero).SetString(xValueStr)\n"+
+      "xValueStr= '%v'\n"+
+      "Error= 'SetString() FAILED!'\n\n", ePrefix, xValueStr)
+    return
+  }
+
+  // Fractional Digits ---------1---------2---------3
+  //         Accuracy: 123456789012345678901234567890
+  expectedResult := "5." +
+    "5012582105447269848114648201125470987997708134322400988314281913493387132117963002054508752689226647" +
+    "8608969143341784472317150974468024988593153280779150727936782857370573281241801155421927627388228010" +
+    "260330798331306088066067384276824977246856395981"
+
+  prec := uint(7000)
+
+  actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)
+
+  if err != nil {
+
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)\n"+
+      "xValue= '%v'\n"+
+      "prec= '%v'\n"+
+      "Error= '%v'\n\n",
+      ePrefix,
+      xValue.Text('f', 30),
+      prec,
+      err.Error())
+
+    return
+  }
+
+  bFloatHlpr := new(BigFloatHelper)
+
+  _, decDigits := bFloatHlpr.CountDigits(expectedResult, '.')
+
+  actualResultStr := actualResult.Text('f', decDigits)
+
+  if expectedResult != actualResultStr {
+    t.Errorf("%v\n"+
+      "Error: Unexpected Result!\n"+
+      "Because expectedResult != actualResultStr\n"+
+      "Expected actualResultStr = '%v'\n"+
+      "  Actual actualResultStr = '%v'\n\n",
+      ePrefix, expectedResult, actualResultStr)
+  }
+
+  return
 }
--- Runes/07_RuneDigits/main.go
+++ Runes/07_RuneDigits/main.go
@@ -4,47 +4,47 @@ import "fmt"
 
 func main() {
 
-  numStr := " 1 2 3 45.123456 78901"
+	numStr := " 1 2 3 45.123456 78901"
 
-  inDigits, decDigits := CountDigits(numStr, '.')
+	inDigits, decDigits := CountDigits(numStr, '.')
 
-  fmt.Printf("\n\n%v\n"+
-    "Integer Digits: %v\n"+
-    "Decimal Digits: %v\n\n",
-    "CountDigits()",
-    inDigits,
-    decDigits)
+	fmt.Printf("\n\n%v\n"+
+		"Integer Digits: %v\n"+
+		"Decimal Digits: %v\n\n",
+		"CountDigits()",
+		inDigits,
+		decDigits)
 
 }
 
 func CountDigits(numStr string, decimalSeparator rune) (intDigits int, decDigits int) {
 
-  intDigits = 0
-  decDigits = 0
+	intDigits = 0
+	decDigits = 0
 
-  var isInt bool
+	var isInt bool
 
-  isInt = true
+	isInt = true
 
-  for _, v := range numStr {
+	for _, v := range numStr {
 
-    if v == decimalSeparator {
-      isInt = false
-      continue
-    }
+		if v == decimalSeparator {
+			isInt = false
+			continue
+		}
 
-    if v >= '0' && v <= '9' {
+		if v >= '0' && v <= '9' {
 
-      if isInt {
-        // Integer digit to left of decimal point
-        intDigits++
-      } else {
-        // Decimal digit to right of decimal point
-        decDigits++
-      }
-    }
+			if isInt {
+				// Integer digit to left of decimal point
+				intDigits++
+			} else {
+				// Decimal digit to right of decimal point
+				decDigits++
+			}
+		}
 
-  }
+	}
 
-  return intDigits, decDigits
+	return intDigits, decDigits
 }
