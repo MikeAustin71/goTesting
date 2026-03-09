d8bfdfe565a9d236f8a2f96b5e2d881678244699
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2026-03-09 02:44:47 -0500

    008Fix_01 Added test xt_naturalLogTaylor_01_test.go - Test_Natural_Logarithm_Taylor_03

--- Math/bFloatNatLog/02NatLog/xt_naturalLogTaylor_01_test.go
+++ Math/bFloatNatLog/02NatLog/xt_naturalLogTaylor_01_test.go
@@ -1,129 +1,239 @@
 package naturalLogCalcs
 
 import (
-	"math/big"
-	"testing"
+  "fmt"
+  "math/big"
+  "testing"
+  "time"
 )
 
 func Test_Natural_Logarithm_Taylor_01(t *testing.T) {
 
-	ePrefix := "Test_Natural_Logarithm_Taylor_01()"
+  ePrefix := "Test_Natural_Logarithm_Taylor_01()"
 
-	xValueStr := "3237"
+  xValueStr := "3237"
 
-	xValue, isOk := new(big.Float).
-		SetMode(big.AwayFromZero).
-		SetString(xValueStr)
+  xValue, isOk := new(big.Float).
+    SetMode(big.AwayFromZero).
+    SetString(xValueStr)
 
-	if !isOk {
-		t.Errorf("%v\n"+
-			"Error returned by:\n"+
-			"xValue, isOk := new(big.Float)\n"+
-			".SetMode(big.AwayFromZero).SetString(xValueStr)\n"+
-			"xValueStr= '%v'\n"+
-			"Error= 'SetString() FAILED!'\n\n", ePrefix, xValueStr)
-		return
-	}
+  if !isOk {
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "xValue, isOk := new(big.Float)\n"+
+      ".SetMode(big.AwayFromZero).SetString(xValueStr)\n"+
+      "xValueStr= '%v'\n"+
+      "Error= 'SetString() FAILED!'\n\n", ePrefix, xValueStr)
+    return
+  }
 
-	// Fractional Digits ---------1---------2---------3
-	//         Accuracy: 123456789012345678901234567890
-	expectedResult := "8.082402253926244350924204901779"
+  // Fractional Digits ---------1---------2---------3
+  //         Accuracy: 123456789012345678901234567890
+  expectedResult := "8.082402253926244350924204901779"
 
-	prec := uint(7000)
+  prec := uint(7000)
 
-	actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)
+  actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)
 
-	if err != nil {
+  if err != nil {
 
-		t.Errorf("%v\n"+
-			"Error returned by:\n"+
-			"actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)\n"+
-			"xValue= '%v'\n"+
-			"prec= '%v'\n"+
-			"Error= '%v'\n\n",
-			ePrefix,
-			xValue.Text('f', 30),
-			prec,
-			err.Error())
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)\n"+
+      "xValue= '%v'\n"+
+      "prec= '%v'\n"+
+      "Error= '%v'\n\n",
+      ePrefix,
+      xValue.Text('f', 30),
+      prec,
+      err.Error())
 
-		return
-	}
+    return
+  }
 
-	actualResultStr := actualResult.Text('f', 30)
+  actualResultStr := actualResult.Text('f', 30)
 
-	if expectedResult != actualResultStr {
-		t.Errorf("%v\n"+
-			"Error: Unexpected Result!\n"+
-			"Because expectedResult != actualResultStr\n"+
-			"Expected actualResultStr = '%v'\n"+
-			"  Actual actualResultStr = '%v'\n\n",
-			ePrefix, expectedResult, actualResultStr)
-	}
+  if expectedResult != actualResultStr {
+    t.Errorf("%v\n"+
+      "Error: Unexpected Result!\n"+
+      "Because expectedResult != actualResultStr\n"+
+      "Expected actualResultStr = '%v'\n"+
+      "  Actual actualResultStr = '%v'\n\n",
+      ePrefix, expectedResult, actualResultStr)
+  }
 
-	return
+  return
 }
 
 func Test_Natural_Logarithm_Taylor_02(t *testing.T) {
 
-	ePrefix := "Test_Natural_Logarithm_Taylor_02()"
+  ePrefix := "Test_Natural_Logarithm_Taylor_02()"
 
-	xValueStr := "245"
+  xValueStr := "245"
 
-	xValue, isOk := new(big.Float).
-		SetMode(big.AwayFromZero).
-		SetString(xValueStr)
+  xValue, isOk := new(big.Float).
+    SetMode(big.AwayFromZero).
+    SetString(xValueStr)
 
-	if !isOk {
-		t.Errorf("%v\n"+
-			"Error returned by:\n"+
-			"xValue, isOk := new(big.Float)\n"+
-			".SetMode(big.AwayFromZero).SetString(xValueStr)\n"+
-			"xValueStr= '%v'\n"+
-			"Error= 'SetString() FAILED!'\n\n", ePrefix, xValueStr)
-		return
-	}
+  if !isOk {
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "xValue, isOk := new(big.Float)\n"+
+      ".SetMode(big.AwayFromZero).SetString(xValueStr)\n"+
+      "xValueStr= '%v'\n"+
+      "Error= 'SetString() FAILED!'\n\n", ePrefix, xValueStr)
+    return
+  }
 
-	// Fractional Digits ---------1---------2---------3
-	//         Accuracy: 123456789012345678901234567890
-	expectedResult := "5." +
-		"5012582105447269848114648201125470987997708134322400988314281913493387132117963002054508752689226647" +
-		"8608969143341784472317150974468024988593153280779150727936782857370573281241801155421927627388228010" +
-		"260330798331306088066067384276824977246856395981"
+  // Fractional Digits ---------1---------2---------3
+  //         Accuracy: 123456789012345678901234567890
+  expectedResult := "5." +
+    "5012582105447269848114648201125470987997708134322400988314281913493387132117963002054508752689226647" +
+    "8608969143341784472317150974468024988593153280779150727936782857370573281241801155421927627388228010" +
+    "260330798331306088066067384276824977246856395981"
 
-	prec := uint(7000)
+  prec := uint(7000)
 
-	actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)
+  actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)
 
-	if err != nil {
+  if err != nil {
 
-		t.Errorf("%v\n"+
-			"Error returned by:\n"+
-			"actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)\n"+
-			"xValue= '%v'\n"+
-			"prec= '%v'\n"+
-			"Error= '%v'\n\n",
-			ePrefix,
-			xValue.Text('f', 30),
-			prec,
-			err.Error())
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)\n"+
+      "xValue= '%v'\n"+
+      "prec= '%v'\n"+
+      "Error= '%v'\n\n",
+      ePrefix,
+      xValue.Text('f', 30),
+      prec,
+      err.Error())
 
-		return
-	}
+    return
+  }
 
-	bFloatHlpr := new(BigFloatHelper)
+  bFloatHlpr := new(BigFloatHelper)
 
-	_, decDigits := bFloatHlpr.CountDigits(expectedResult, '.')
+  _, decDigits := bFloatHlpr.CountDigits(expectedResult, '.')
 
-	actualResultStr := actualResult.Text('f', decDigits)
+  actualResultStr := actualResult.Text('f', decDigits)
 
-	if expectedResult != actualResultStr {
-		t.Errorf("%v\n"+
-			"Error: Unexpected Result!\n"+
-			"Because expectedResult != actualResultStr\n"+
-			"Expected actualResultStr = '%v'\n"+
-			"  Actual actualResultStr = '%v'\n\n",
-			ePrefix, expectedResult, actualResultStr)
-	}
+  if expectedResult != actualResultStr {
+    t.Errorf("%v\n"+
+      "Error: Unexpected Result!\n"+
+      "Because expectedResult != actualResultStr\n"+
+      "Expected actualResultStr = '%v'\n"+
+      "  Actual actualResultStr = '%v'\n\n",
+      ePrefix, expectedResult, actualResultStr)
+  }
 
-	return
+  return
+}
+
+func Test_Natural_Logarithm_Taylor_03(t *testing.T) {
+
+  ePrefix := "Test_Natural_Logarithm_Taylor_03()"
+
+  xValueStr := "1.0000001"
+
+  bigFloatHlpr := new(BigFloatHelper)
+
+  //prec := uint(7000)
+  // Precision set for approximately
+  // 2,108 Decimal Digits of Accuracy
+
+  prec := bigFloatHlpr.ComputeBigFloatPrecisionBits(55, 0)
+
+  xValue, isOk := new(big.Float).
+    SetMode(big.AwayFromZero).
+    SetPrec(prec).
+    SetString(xValueStr)
+
+  if !isOk {
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "xValue, isOk := new(big.Float)\n"+
+      ".SetMode(big.AwayFromZero).SetString(xValueStr)\n"+
+      "xValueStr= '%v'\n"+
+      "Error= 'SetString() FAILED!'\n\n", ePrefix, xValueStr)
+    return
+  }
+
+  // Copilot expected result "0.000000099999995000000333333341666666708"
+  expectedResult :=
+    "0.000000099999995000000333333308333335333333166666681"
+  //   123456789012345678901234567890123456789012345678901
+  //   ---------1---------2---------3---------4---------5
+  //     39- Decimal Digits of Accuracy
+
+  startTime := time.Now()
+
+  actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)
+
+  if err != nil {
+
+    t.Errorf("%v\n"+
+      "Error returned by:\n"+
+      "actualResult, err := new(naturalLogTaylor).lnTaylorDirect(xValue, prec)\n"+
+      "xValue= '%v'\n"+
+      "prec= '%v'\n"+
+      "Error= '%v'\n\n",
+      ePrefix,
+      xValue.Text('f', 30),
+      prec,
+      err.Error())
+
+    return
+  }
+
+  endTime := time.Now()
+
+  funcExecutionTime := bigFloatHlpr.DurationBreakdown(startTime, endTime)
+
+  _, decDigitsOfExpectedPrecision := bigFloatHlpr.CountDigits(expectedResult, '.')
+
+  actualResultStr := actualResult.Text('f', decDigitsOfExpectedPrecision)
+
+  if expectedResult != actualResultStr {
+    t.Errorf("%v\n"+
+      "Error: Unexpected Result!\n"+
+      "Because expectedResult != actualResultStr\n"+
+      "Expected actualResultStr = '%v'\n"+
+      "  Actual actualResultStr = '%v'\n"+
+      "XValue = '%v'\n"+
+      "Big Float Precision Bits - prec = '%v'\n"+
+      "Decimal Digits of Precision = '%v'\n"+
+      "naturalLogTaylor.lnTaylorDirect() Execution Time: %v\n\n",
+      ePrefix,
+      expectedResult,
+      actualResultStr,
+      xValueStr,
+      prec,
+      decDigitsOfExpectedPrecision, funcExecutionTime)
+
+    return
+  }
+
+  decDigitsOfInputAccuracy := new(BigFloatHelper).ComputeBigFloatDecimalDigits(
+    prec, 0)
+
+  fmt.Printf("\n\n%v\n"+
+    "Successful Completion !\n"+
+    "expected Result = '%v'\n"+
+    "  actual Result = '%v'\n"+
+    "XValue = '%v'\n"+
+    "     Big Float Precision Bits - prec = '%v'\n"+
+    "   Decimal Digits of Input Precision = '%v'\n"+
+    "Decimal Digits of Expected Precision = '%v'\n"+
+    "naturalLogTaylor.lnTaylorDirect() Execution Time: %v\n\n",
+    ePrefix,
+    expectedResult,
+    actualResultStr,
+    xValueStr,
+    prec,
+    decDigitsOfInputAccuracy,
+    decDigitsOfExpectedPrecision,
+    funcExecutionTime)
+
+  return
 }
--- Math/bFloatNatLog/02NatLog/xt_naturalLogTaylor_03_test.go
+++ Math/bFloatNatLog/02NatLog/xt_naturalLogTaylor_03_test.go
@@ -1,52 +1,52 @@
 package naturalLogCalcs
 
 import (
-  "math/big"
-  "testing"
+	"math/big"
+	"testing"
 )
 
 // 007Fix
 func Test_Natural_Logarithm_Taylor_NearOne(t *testing.T) {
 
-  tests := []struct {
-    xStr     string
-    expected string
-  }{
-    // Very close to 1
-    {"1.0000001", "0.000000099999995000000333333341666666708"},
-    {"1.000001", "0.000000999999500000333333416666708333208"},
-    {"1.00001", "0.000009999950000333341666708333208333208"},
-    {"1.0001", "0.000099995000333308335333166680951"},
-    {"1.001", "0.000999500333083423333416708333208"},
-  }
+	tests := []struct {
+		xStr     string
+		expected string
+	}{
+		// Very close to 1
+		{"1.0000001", "0.000000099999995000000333333341666666708"},
+		{"1.000001", "0.000000999999500000333333416666708333208"},
+		{"1.00001", "0.000009999950000333341666708333208333208"},
+		{"1.0001", "0.000099995000333308335333166680951"},
+		{"1.001", "0.000999500333083423333416708333208"},
+	}
 
-  prec := uint(7000)
-  taylor := new(naturalLogTaylor)
-  bFloatHlpr := new(BigFloatHelper)
+	prec := uint(7000)
+	taylor := new(naturalLogTaylor)
+	bFloatHlpr := new(BigFloatHelper)
 
-  for _, tc := range tests {
+	for _, tc := range tests {
 
-    x, ok := new(big.Float).
-      SetMode(big.AwayFromZero).
-      SetString(tc.xStr)
+		x, ok := new(big.Float).
+			SetMode(big.AwayFromZero).
+			SetString(tc.xStr)
 
-    if !ok {
-      t.Errorf("FAILED: SetString(%v)", tc.xStr)
-      continue
-    }
+		if !ok {
+			t.Errorf("FAILED: SetString(%v)", tc.xStr)
+			continue
+		}
 
-    actual, err := taylor.lnTaylorDirect(x, prec)
-    if err != nil {
-      t.Errorf("Error computing ln(%v): %v", tc.xStr, err)
-      continue
-    }
+		actual, err := taylor.lnTaylorDirect(x, prec)
+		if err != nil {
+			t.Errorf("Error computing ln(%v): %v", tc.xStr, err)
+			continue
+		}
 
-    _, decDigits := bFloatHlpr.CountDigits(tc.expected, '.')
-    actualStr := actual.Text('f', decDigits)
+		_, decDigits := bFloatHlpr.CountDigits(tc.expected, '.')
+		actualStr := actual.Text('f', decDigits)
 
-    if actualStr != tc.expected {
-      t.Errorf("\nln(%v) mismatch\nExpected: %v\nActual:   %v\n",
-        tc.xStr, tc.expected, actualStr)
-    }
-  }
+		if actualStr != tc.expected {
+			t.Errorf("\nln(%v) mismatch\nExpected: %v\nActual:   %v\n",
+				tc.xStr, tc.expected, actualStr)
+		}
+	}
 }
